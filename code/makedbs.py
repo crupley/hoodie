28b7ab2018a2f1d568f9b4c380648f1ffbcb2ef6
crupley <crupley@gmail.com>
2015-11-24 13:50:41 -0800

    integrate code from scrape.py

--- code/makedbs.py
+++ code/makedbs.py
@@ -5,6 +5,7 @@ import re
 import psycopg2
 from sqlalchemy import create_engine
 from sqlalchemy.engine.url import URL
+import requests
 
 from pyshp import shapefile
 
@@ -747,7 +748,63 @@ def make_usc_shapefile():
 
 
 # Walkscore
-# imported in scrape.py
+def scrape_walkscore(lat, lon):
+    """
+    API query to walkscore.com for walkscore data nearest to a particular
+    latitude and longitude. Walkscore snaps to a grid with 500 ft. spacing.
+
+    Args:
+      lat, lon: latitude and longitude; float
+    Returns:
+      None; stores results in postgres database
+    """
+    with open('/Users/crupley/.api/walkscore.txt') as f:
+        wskey = f.readline().strip()
+
+    requrl = 'http://api.walkscore.com/score'
+    payload = {'wsapikey': wskey,
+               'lat': lat,
+               'lon': lon,
+               'format': 'json'}
+
+    response = requests.get(requrl, params=payload)
+
+    if response.json()['status'] != 1:
+        print 'Server response error, code:', response.json()['status']
+        return
+
+    data = pd.Series(response.json())
+    data['searched_lat'] = lat
+    data['searched_lon'] = lon
+
+    ncols = data.shape[0]
+    q_string = '''
+      INSERT INTO walkscore_raw (description,
+                     help_link,
+                     logo_url,
+                     more_info_icon,
+                     more_info_link,
+                     snapped_lat,
+                     snapped_lon,
+                     status,
+                     updated,
+                     walkscore,
+                     ws_link,
+                     searched_lat,
+                     searched_lon)
+        VALUES (%s)''' % ('%s, ' * (ncols - 1) + '%s')
+
+    conn = psycopg2.connect(dbname='hoodie', user='postgres', host='/tmp')
+    c = conn.cursor()
+
+    c.execute(q_string, data.values)
+
+    conn.commit()
+    conn.close()
+
+    return
+
+
 def clean_walkscore(df):
     """
     Cleans 'walkscore' data
