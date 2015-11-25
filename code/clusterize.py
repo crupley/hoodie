8aaa3d5ae644e61e614ee6fcf7d1bc97271c4050
crupley <crupley@gmail.com>
2015-11-25 12:13:12 -0800

    add all docstrings

--- code/clusterize.py
+++ code/clusterize.py
@@ -142,74 +142,59 @@ def assign_clusters(nodelist, graph):
 
 
 def row_errorsq(row, cluster_means):
+	"""
+	Calculate the feature distance between each node and its cluster mean,
+	to be used in an .apply to pandas dataframe
+
+	Args:
+		row : row of a pandas dataframe containing columns of features and one of assigned cluster number designated 'cnum'
+		cluster_means : pandas dataframe of the mean of each feature in 'row'
+			indexed by cluster number.
+	Returns:
+		feature distance, float
+	"""
     rowf = row.drop(['cnum'])
     return (fdist(rowf, cluster_means.ix[int(row.cnum)]))
 
 
 def wcss(featuredf, cnum):
+	"""
+	Calculate the within cluster sum of squares error (wcss)
+
+	Args:
+		featuredf : pandas dataframe of features
+		cnum : assigned cluster numbers, same length as featuredf,
+			list-like of int
+	Returns:
+		wcss error, float
+	"""
     df = featuredf.copy()
-    df['cnum'] = cnum    # features = df.drop(['lat', 'lon'], axis = 1)
+    df['cnum'] = cnum
     cluster_means = df.groupby('cnum').mean()
     df['errors'] = df.apply(lambda x: row_errorsq(x, cluster_means),
     							axis = 1)
     return df.groupby('cnum').sum()['errors']
 
 
-def elbow_plot(cutdf, featuredf, maxk=30):
-	'''
-	calculate wcss vs k
-
-	cutdf: list of cuts containing columns 'source', 'target', and 'nclusters'
-
-	'''
-
-	ks = []
-	wcsses = []
-
-	graph = make_graph(cutdf)
-
-	nclusterslast = nx.number_connected_components(graph)
-	# ks.append(nclusterslast)
-	# wcsses
-
-	for i in cutdf.index:
-		nclusters = cutcon(cutdf.ix[i], graph)
-		if nclusters > nclusterslast:
-			# print i, nclusters
-			nclusterslast = nclusters
-			ks.append(nclusters)
-			cnum = assign_clusters(featuredf.index, graph)
-			wcsses.append(wcss(featuredf, cnum).sum())
-			if nclusters >= maxk: return ks, wcsses
-
-
-	# for k in ks:
-	#     # rebuild full graph
-	#     #g = nx.from_pandas_dataframe(edges, 'source', 'target')
-	#     g = graph.copy()
-
-	#     # reduces graph to i clusters
-	#     cutdf[cutdf.nclusters <= k].apply(lambda x: cutrow(x, g), axis=1)
-
-	#     # assign cluster numbers
-	#     featuredf['cnum'] = assign_clusters(featuredf.index, g)
-
-	#     wcss = wcss(featuredf)
-	#     wcsses.append(wcss.ix[wcss.index >= 0].sum())
-	#     print 'k:', k, 'WCSSE:', wcss.ix[wcss.index >= 0].sum(),
-	#     print g.number_of_edges()
-
-	return ks, wcsses
-
-
 def cut2cluster(featurelist, nclusters, allowed_nodes=None):
-	'''
-	returns Series with cluster number indexed by node number
+	"""
+	Using a list of cuts from a betweenness similarity graph reduction,
+	returns the assigned cluster number for each node for the given
+	number of clusters.
 
-	featurelist: '010507'
-	nclusters: int
-	'''
-
+	Args:
+		featurelist : combination of features to use, string of numbers
+			designating features from FDICT in form 'f1f2f3', e.g. '010507'
+		nclusters : number of clusters to split nodes into, int. Minimum
+			is the starting number of clusters of the full graph. If nclusters
+			is greater than number of nodes in graph, will assign each
+			node to unique cluster
+		allowed_nodes : node numbers of allowed nodes if only
+		certain nodes are to be considered, list-like of ints
+	Returns:
+		Assigned cluster numbers, pandas Series indexed by node number, ints
+	"""
+	# load from cut list file
 	fn = 'results/CL' + featurelist + '.csv'
 	edges = pd.read_csv(fn)
 	edges = edges[['source', 'target']]
@@ -220,17 +205,7 @@ def cut2cluster(featurelist, nclusters, allowed_nodes=None):
 		ncc = cutcon(edges.ix[i], graph)
 		if ncc >= nclusters: break
 
-
-	# # add count of number of clusters after each cut
-	# g = graph.copy()
-	# edges['nclusters'] = edges.apply(lambda x: cutcon(x, g), axis=1)
-
-	# # reduces graph to i clusters
-	# g = graph.copy()
-	# edges[edges.nclusters <= nclusters].apply(lambda x: cutrow(x, g), axis=1)
-
 	nodeset = set(edges.source).union(set(edges.target))
-
 	
 	if allowed_nodes is not None:
 		# remove unallowed nodes from nodelist
@@ -245,9 +220,27 @@ def cut2cluster(featurelist, nclusters, allowed_nodes=None):
 	return assign_clusters(nodelist, graph)
 
 def feature_bars(featuredf, cnum, plot=False, **kwargs):
-	'''
-	barplot of features grouped by cluster number
-	'''
+	"""
+	Normalizes features to zero mean and unit range for use in bar plots.
+	Can optionally produce bar plot.
+
+	Args:
+		featuredf : features to be scaled indexed by cluster number,
+			length: number of nodes,
+			width: number of features,
+			pandas dataframe
+		cnum : assigned cluster number for each node
+			length: number of nodes
+			list-like of ints
+		plot : boolean, create barplot of scaled features
+		kwargs : additional arguments to be passed to 
+			pandas dataframe.plot(kind='bar')
+	Returns:
+		scaled feature value for each feature and cluster
+			length : number of clusters
+			width : number of features
+			pandas dataframe
+	"""
 	df = featuredf.copy()
 	df['cnum'] = cnum
 
@@ -264,9 +257,20 @@ def feature_bars(featuredf, cnum, plot=False, **kwargs):
 
 
 def most_similar(featuredf, cluster_labels):
-	'''
-	returns feature distance from cluster_number to all others
-	'''
+	"""
+	Pairwise feature comparison between each cluster
+
+	Args:
+		featuredf : node features,
+			length: number of nodes,
+			width: number of features,
+			pandas dataframe
+		cluster_labels : assigned cluster number for each node
+			length: number of nodes
+			list-like of ints
+	Returns:
+		Feature distances, pandas dataframe, n clusters x n clusters
+	"""
 	cluster_means = featuredf.groupby(cluster_labels).mean()
 	df =  pd.DataFrame(pairwise_distances(cluster_means,
 						metric='l2'),
@@ -276,24 +280,144 @@ def most_similar(featuredf, cluster_labels):
 
 
 def gencolors(n, cmap='jet'):
+	"""Generates list of hex colors n long in given matplotlib colormap"""
 	c = matplotlib.cm.get_cmap('Set1')
 	clist = [matplotlib.colors.rgb2hex(rgb) for rgb in c(np.linspace(0,1,n))]
 	return clist
 
 
 def to_rghex(n):
+	"""Converts number, [0-1], into hex color on green-to-red gradient"""
     return matplotlib.colors.rgb2hex([n, 1-n, 0, 1])
 
 
 def rg_colormatrix(sim):
+	"""
+	Creates red-green matrix of colors from similarity matrix
+	covering the range of distances in the similarity matrix
+
+	Args:
+		sim : similarity matrix as created by most_similar, pandas dataframe
+	Returns:
+		matrix of hex colors of same dimension as sim, pandas dataframe
+	"""
+	# normalize similarity values 0-1
     normed = sim / sim.max().max()
+    # convert each 0-1 value to hex color on green-to-red gradient
     return normed.applymap(to_rghex)
 
 
-def list_(*args): return list(args)
+def list_(*args):
+	"""Flattens list of lists"""
+	return list(args)
+
+
+def merge_map_data(path, featuredf, store=False):
+	"""
+	For each set of features, take results of graph reduction,
+	assign cluster numbers, and compute all other data for insertion
+	into final geojson file.
+
+	Args:
+		path : path to directory containing graph cut list files, string
+		featuredf : scaled features, pandas dataframe
+	Returns:
+		Data for insertion into geojson, pandas dataframe
+	"""
+	# get filenames
+	files = os.listdir(path)
+	files = [f[2:-4] for f in files if f[:2] == 'CL']
+
+	# null map
+	files.remove('xx')
+
+	# only allow 3 or less features
+	mapnos = [f for f in files if len(f) <= 6]
+
+	fnums = [mapno2list(f) for f in mapnos]
+
+	# column names
+	fnames = map(lambda x: [FDICT[n] for n in x], fnums)
+
+	# fixed number of clusters
+	nclustersmax = 28
+
+	### make null map
+	cnum = cut2cluster('xx', nclustersmax, allowed_nodes=featuredf.index)
+
+	# retain only mutual nodes
+	nodelist = set(featuredf.index).intersection(set(cnum.index))
+	featuredf = featuredf.ix[nodelist]
+	cnum = cnum.ix[nodelist]
+	nclusters = len(cnum.unique())
+
+	# compute data
+
+	# similarity colors
+	rgmatrix = rg_colormatrix(most_similar(featuredf, cnum))
+	# feature bar graph data
+	fbars = feature_bars(featuredf[FDICT.values()], cnum)
+
+	# shape file polygons
+	fn = 'data/uscensus/tl_2010_06075_tabblock10/tl_2010_06075_tabblock10.dbf'
+	mergedf = merge_shapefiles(featuredf[['lat', 'lon']], fn)
+	polys = make_shapefiles(featuredf[['lat', 'lon']], mergedf.polys, cnum)
+
+	# compile into single dataframe
+	alldf = pd.DataFrame({'cnum': cnum.unique(),
+                      'polygon': polys})
+	alldf['rgmatrix'] = map(lambda x: list(rgmatrix.ix[x]), cnum.unique())
+	alldf['mapno'] = ''
+	alldf['fbars'] = map(list, fbars.round(2).values)
+
+	# store results
+	if store:
+		alldf.to_csv('results/alldf.csv')
+
+	# make all other maps
+	for i, f in enumerate(mapnos):
+		cnum = cut2cluster(f, nclustersmax, allowed_nodes=featuredf.index)
+		rgmatrix = rg_colormatrix(most_similar(featuredf, cnum))
+
+		fbars = feature_bars(featuredf[fnames[i]], cnum)
+		polys = make_shapefiles(featuredf[['lat', 'lon']],
+								mergedf.polys, cnum)
+
+		onedf = pd.DataFrame({'cnum': cnum.unique(),
+                      'polygon': polys})
+		onedf['rgmatrix'] = map(lambda x: list(rgmatrix.ix[x]), cnum.unique())
+		onedf['mapno'] = f
+		onedf['fbars'] = map(list, fbars.round(2).values)
+
+		# append results after each map
+		if store:
+			with open('results/alldf.csv', 'a') as storefile:
+			    onedf.to_csv(storefile, header=False)
+
+		alldf = pd.concat((alldf, onedf), axis=0, ignore_index=True)
+
+	return alldf
 
 
 def make_json(cnum, polys, rgmatrix, mapno, fbars):
+	"""
+	Take data from merge_map_data and convert to geojson format
+
+	Args:
+		cnum : assigned cluster number, pandas Series/dataframe of ints
+		polys : shapely polygon for each cluster,
+			pandas Series/dataframe of shapely objects
+		rgmatrix : hex colors for each cluster,
+			pandas Series/dataframe of lists each length n clusters
+		mapno : assigned map number of each cluster,
+			pandas Series/dataframe of str
+		fbars : data for feature bar graph, pandas Series/dataframe
+			of list of float, length n features
+
+		All args of length number of clusters
+	Returns:
+		geojson-formatted dict
+	"""
 	
 	# column names
 	fnamesc = map(lambda x: [FDICT[n] for n in mapno2list(x)], mapno)
@@ -306,14 +430,13 @@ def make_json(cnum, polys, rgmatrix, mapno, fbars):
 	for i in xrange(len(cnum)):
 	    featurelist.append({"type": "Feature",
 	                        "properties": {
-	                        # "color": clist.iloc[i],
-	                        "rgmat": rgmatrix.iloc[i],
-	                        "mapno": mapno.iloc[i],
-	                        "neibno": cnum.iloc[i],
-	                        "bars" : map(list_, fnames[i],
-	                        			 fbars.iloc[i]),
-	                        "visible": False
-	                        },
+		                        "rgmat": rgmatrix.iloc[i],
+		                        "mapno": mapno.iloc[i],
+		                        "neibno": cnum.iloc[i],
+		                        "bars" : map(list_, fnames[i],
+		                        			 fbars.iloc[i]),
+		                        "visible": False
+	                        	},
 	                        "geometry": mapping(polys.iloc[i])
 	                        })
 	    
@@ -323,84 +446,15 @@ def make_json(cnum, polys, rgmatrix, mapno, fbars):
 	return geojson
 
 
-def merge_map_data(path, featuredf, store=False):
-	files = os.listdir(path)
-	files = [f[2:-4] for f in files if f[:2] == 'CL']
-	files.remove('xx')
-
-	# incomplete cut list
-	files.remove('000104')
-
-	mapnos = [f for f in files if len(f) <= 6]
-	# mapnos = ['010405']
-
-	fnums = [mapno2list(f) for f in mapnos]
-
-	# column names
-	fnames = map(lambda x: [FDICT[n] for n in x], fnums)
-
-	nclustersmax = 28
-
-	### make null map
-	cnum = cut2cluster('xx', nclustersmax, allowed_nodes=featuredf.index)
-
-	# retain only mutual nodes
-	nodelist = set(featuredf.index).intersection(set(cnum.index))
-	featuredf = featuredf.ix[nodelist]
-	cnum = cnum.ix[nodelist]
-	nclusters = len(cnum.unique())
-
-	# clist = gencolors(nclusters)
-	rgmatrix = rg_colormatrix(most_similar(featuredf, cnum))
-	fbars = feature_bars(featuredf[FDICT.values()], cnum)
-
-	fn = 'data/uscensus/tl_2010_06075_tabblock10/tl_2010_06075_tabblock10.dbf'
-	mergedf = merge_shapefiles(featuredf[['lat', 'lon']], fn)
-	polys = make_shapefiles(featuredf[['lat', 'lon']], mergedf.polys, cnum)
-
-	# pdb.set_trace()
-
-	alldf = pd.DataFrame({'cnum': cnum.unique(),
-                      'polygon': polys})
-	# alldf['color'] = clist
-	alldf['rgmatrix'] = map(lambda x: list(rgmatrix.ix[x]), cnum.unique())
-	alldf['mapno'] = ''
-	alldf['fbars'] = map(list, fbars.round(2).values)
-
-	# store results
-	if store:
-		alldf.to_csv('results/alldf.csv')
-
-	# make all other maps
-	for i, f in enumerate(mapnos):
-		print f
-		cnum = cut2cluster(f, nclustersmax, allowed_nodes=featuredf.index)
-		rgmatrix = rg_colormatrix(most_similar(featuredf, cnum))
-
-		# cnum = cnum.ix[nodelist]
-
-		fbars = feature_bars(featuredf[fnames[i]], cnum)
-		polys = make_shapefiles(featuredf[['lat', 'lon']],
-								mergedf.polys, cnum)
-
-		onedf = pd.DataFrame({'cnum': cnum.unique(),
-                      'polygon': polys})
-
-		# onedf['color'] = clist
-		onedf['rgmatrix'] = map(lambda x: list(rgmatrix.ix[x]), cnum.unique())
-		onedf['mapno'] = f
-		onedf['fbars'] = map(list, fbars.round(2).values)
-
-		if store:
-			with open('results/alldf.csv', 'a') as storefile:
-			    onedf.to_csv(storefile, header=False)
-
-		alldf = pd.concat((alldf, onedf), axis=0, ignore_index=True)
-
-	return alldf
-
-
 def load_featuredf():
+	"""
+	Loads feature dataframe and removes unwanted data
+
+	Args:
+		None
+	Returns:
+		feature pandas dataframe
+	"""
 	with open('featuresdf.pkl', 'rb') as f:
 		fdf = pickle.load(f)
 
@@ -416,7 +470,7 @@ def load_featuredf():
 if __name__ == '__main__':
 	fdf = load_featuredf()
 
-	# may take some time
+	# may take a while
 	alldf = merge_map_data('results', fdf, store=True)
 
 	gjson = make_json(alldf.cnum, alldf.polygon, alldf.color,
